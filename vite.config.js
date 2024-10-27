83ba19724516bbd61104484b78977472a2783b88
egai <egaiosowor@yahoo.com>
2024-10-27 08:21:53 -0700

    migrate to pwa

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,55 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from "tailwindcss";
 import { resolve } from 'path';
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+    registerType:'prompt',
+    includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+    manifest:{
+      name:"Taask",
+      short_name:"Taask",
+      description:"Taask - A Task management PWA built using ReactJS",
+      icons:[{
+        src: '/icons/192.png',
+        sizes:'192x192',
+        type:'image/png',
+        purpose:'favicon'
+      },
+      {
+        src:'/icons/512.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'favicon'
+      },
+      {
+        src: '/icons/180.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
+      },
+      {
+        src: '/icon512_maskable.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'any maskable',
+      }
+    ],
+    theme_color:'#171717',
+    background_color:'#f0e7db',
+    display:"standalone",
+    scope:'/',
+    start_url:"/",
+    orientation:'portrait'
+    }
+  }
 
 export default defineConfig({
     server: {
         port: 3000
     },
 
-    plugins: [react()],
+    plugins: [react(), VitePWA(manifestForPlugIn)],
 
     css: {
         postcss: {
