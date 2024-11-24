d68bd482d6558040858e026c1af92f73f6d9ee52
egai <egaiosowor@yahoo.com>
2024-11-24 19:32:17 +0100

    make noproject and notask pages mobile responsive

--- src/components/ui/NoProject.jsx
+++ src/components/ui/NoProject.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { CiFolderOff } from "react-icons/ci";
 import { AddEditProjectModal } from "./modals";
-import { useDispatch, useSelector } from "react-redux";
 
 function NoProject() {
   
   const [isProjectModalOpen, setIsProjectModalOpen] = useState(false);
   
-  const projects = useSelector((state) => state.projects);
-  
   return (
     <div className="bg-[#1F1F1F] h-full grid justify-center items-center px-2 flex-[5] rounded-[7px] text-white" >
         <div className='flex items-center space-y-4' > 
           <div className='flex flex-col items-center space-y-4'>
-            <CiFolderOff className='text-[120px]' />
-            <p className='text-4xl' >No Projects Available</p>
-            <a 
+            <CiFolderOff className='text-[80px] md:text-[120px]' />
+            <p className='text-2xl md:text-4xl' >No Projects Available</p>
+            <p
               onClick={() => {
                 setIsProjectModalOpen(true);
               }}
-              href='/create-task' 
-              className='underline hover:no-underline text-base text-[#C38FFF]' >Add Project</a>
+              className='underline hover:no-underline text-base text-[#C38FFF] cursor-pointer' >Add Project</p>
           </div> 
         </div>
         {isProjectModalOpen && (
--- src/components/ui/NoTask.jsx
+++ src/components/ui/NoTask.jsx
@@ -8,8 +8,8 @@ function NoTask() {
     <div className="bg-[#1F1F1F] h-full grid justify-center items-center px-2 flex-[5] rounded-[7px] text-white" >
         <div className='flex items-center space-y-4' > 
           <div className='flex flex-col items-center space-y-4'>
-            <BiTaskX className='text-[120px]' />
-            <p className='text-4xl' >No Tasks Available</p>
+            <BiTaskX className='text-[80px] md:text-[120px]' />
+            <p className='text-2xl md:text-4xl' >No Tasks Available</p>
             <a href='/create-task' className='underline hover:no-underline text-base text-[#C38FFF]' >Add Task</a>
           </div> 
         </div>
